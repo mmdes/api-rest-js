71652e12ba71d9839b7f09a6f254d39764571b1a
Matheus Matos <57081161+mmdes@users.noreply.github.com>
2024-08-16 13:41:56 -0400

    Finalizando CRUD para o User

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -46,8 +46,53 @@ class UserController {
   async store(req, res) {
     try {
       const newUser = await User.create(req.body);
-      const { id, nome, email } = newUser;
-      return res.json({ id, nome, email });
+      const { id, name, email } = newUser;
+      return res.json({ id, name, email });
+    } catch (e) {
+      if (e.errors && e.errors.length > 0) {
+        return res.status(400).json({
+          errors: e.errors.map((err) => err.message),
+        });
+      } if (e.parent && e.parent.sqlMessage) {
+        return res.status(400).json({
+          errors: e.parent.sqlMessage,
+        });
+      }
+      return res.status(400).json(null);
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
+      if (!user) {
+        return res.status(400).json({
+          errors: ['User does not exist.'],
+        });
+      }
+      const newData = await user.update(req.body);
+      const { id, name, email } = newData;
+      return res.json({ id, name, email });
+    } catch (e) {
+      if (e.errors && e.errors.length > 0) {
+        return res.status(400).json({
+          errors: e.errors.map((err) => err.message),
+        });
+      } if (e.parent && e.parent.sqlMessage) {
+        return res.status(400).json({
+          errors: e.parent.sqlMessage,
+        });
+      }
+      return res.status(400).json(null);
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const user = await User.findByPk(req.params.id);
+      const { id, name, email } = user;
+      user.destroy();
+      return res.json({ id, name, email });
     } catch (e) {
       if (e.errors && e.errors.length > 0) {
         return res.status(400).json({
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -6,5 +6,7 @@ const router = new Router();
 router.get('/', userController.index);
 router.get('/:id', userController.show);
 router.post('/', userController.store);
+router.put('/:id', userController.update);
+router.delete('/:id', userController.delete);
 
 export default router;
